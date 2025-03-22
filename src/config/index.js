0ae3c2a8bb8e93893a961e49e1e80ac4cbf8f691
anveloper <hitedin@gmail.com>
2025-03-22 21:40:59 +0900

    feat: 전역 상수 정의 및 재사용

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  MAIN: "/",
+  LOGIN: "/login",
+  LOGOUT: "/logout",
+  PROFILE: "/profile",
+  ERROR: "/error",
+};
+
+export const CUSTOM_EVENT = {
+  PAGE_PUSH: "page-push",
+};
