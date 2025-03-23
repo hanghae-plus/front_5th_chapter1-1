bde62685c231a7df1552319d1e18181ec1e08ec4
KumJungMin <rmawjdals@naver.com>
2025-03-23 20:21:16 +0900

    test: 테스트 오류 수정하기

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -21,39 +21,15 @@ const ProfileContent = {
               </h2>
               <form id="profile-form">
                 <div class="mb-4">
-                  <label
-                    for="username"
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    >사용자 이름</label
-                  >
-                  <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    value="${userInfo.username || ""}"
-                    class="w-full p-2 border rounded"
-                  />
+                  <label for="username" class="block text-gray-700 text-sm font-bold mb-2">사용자 이름</label>
+                  <input type="text" id="username" name="username" value="${userInfo.username || ""}" class="w-full p-2 border rounded" />
                 </div>
                 <div class="mb-4">
-                  <label
-                    for="username"
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    >이메일</label
-                  >
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value="${userInfo.email || ""}"
-                    class="w-full p-2 border rounded"
-                  />
+                  <label for="email" class="block text-gray-700 text-sm font-bold mb-2">이메일</label>
+                  <input type="email" id="email" name="email" value="${userInfo.email || ""}" class="w-full p-2 border rounded" />
                 </div>
                 <div class="mb-6">
-                  <label
-                    for="bio"
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    >자기소개</label
-                  >
+                  <label for="bio" class="block text-gray-700 text-sm font-bold mb-2">자기소개</label>
                   <textarea
                     id="bio"
                     name="bio"
