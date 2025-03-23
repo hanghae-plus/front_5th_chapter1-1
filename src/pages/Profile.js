a2259f7600caee35f0b5d25370078544558be328
KumJungMin <rmawjdals@naver.com>
2025-03-23 19:49:07 +0900

    feat: 프로필 노출, 수정 기능 추가하기

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,73 +1,94 @@
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
+import { useUserStore } from "../stores/user.js";
+import { defineComponent } from "../helpers/component";
 
-export default function ProfilePage() {
-  return `
-  <div id="root">
-    <div class="bg-gray-100 min-h-screen flex justify-center">
-      <div class="max-w-md w-full">
-        ${Header()}
-        <main class="p-4">
-          <div class="bg-white p-8 rounded-lg shadow-md">
-            <h2 class="text-2xl font-bold text-center text-blue-600 mb-8">
-              내 프로필
-            </h2>
-            <form>
-              <div class="mb-4">
-                <label
-                  for="username"
-                  class="block text-gray-700 text-sm font-bold mb-2"
-                  >사용자 이름</label
+const ProfileContent = {
+  name: "Profile",
+  components: [Header, Footer],
+  template: ({ children }) => {
+    const userStore = useUserStore();
+    const userInfo = userStore.userInfo;
+    return `
+    <div id="root">
+      <div class="bg-gray-100 min-h-screen flex justify-center">
+        <div class="max-w-md w-full">
+          ${children.Header()}
+          <main class="p-4">
+            <div class="bg-white p-8 rounded-lg shadow-md">
+              <h2 class="text-2xl font-bold text-center text-blue-600 mb-8">
+                내 프로필
+              </h2>
+              <form id="profile-form">
+                <div class="mb-4">
+                  <label
+                    for="username"
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    >사용자 이름</label
+                  >
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value="${userInfo.username || ""}"
+                    class="w-full p-2 border rounded"
+                  />
+                </div>
+                <div class="mb-4">
+                  <label
+                    for="username"
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    >이메일</label
+                  >
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value="${userInfo.email || ""}"
+                    class="w-full p-2 border rounded"
+                  />
+                </div>
+                <div class="mb-6">
+                  <label
+                    for="bio"
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    >자기소개</label
+                  >
+                  <textarea
+                    id="bio"
+                    name="bio"
+                    rows="4"
+                    class="w-full p-2 border rounded"
+                  >${userInfo.bio || ""}</textarea>
+                </div>
+                <button
+                  type="submit"
+                  class="w-full bg-blue-600 text-white p-2 rounded font-bold"
                 >
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  value="홍길동"
-                  class="w-full p-2 border rounded"
-                />
-              </div>
-              <div class="mb-4">
-                <label
-                  for="email"
-                  class="block text-gray-700 text-sm font-bold mb-2"
-                  >이메일</label
-                >
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value="hong@example.com"
-                  class="w-full p-2 border rounded"
-                />
-              </div>
-              <div class="mb-6">
-                <label
-                  for="bio"
-                  class="block text-gray-700 text-sm font-bold mb-2"
-                  >자기소개</label
-                >
-                <textarea
-                  id="bio"
-                  name="bio"
-                  rows="4"
-                  class="w-full p-2 border rounded"
-                >
-안녕하세요, 항해플러스에서 열심히 공부하고 있는 홍길동입니다.</textarea
-                >
-              </div>
-              <button
-                type="submit"
-                class="w-full bg-blue-600 text-white p-2 rounded font-bold"
-              >
-                프로필 업데이트
-              </button>
-            </form>
-          </div>
-        </main>
-        ${Footer()}
+                  프로필 업데이트
+                </button>
+              </form>
+            </div>
+          </main>
+          ${children.Footer()}
+        </div>
       </div>
     </div>
-  </div>
-`;
-}
+  `;
+  },
+  domEvent: () => {
+    const userStore = useUserStore();
+    const profileForm = document.querySelector("#profile-form");
+
+    profileForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const username = profileForm.querySelector("#username").value;
+      const email = profileForm.querySelector("#email").value;
+      const bio = profileForm.querySelector("#bio").value;
+
+      userStore.setUserInfo({ username, email, bio });
+    });
+  },
+};
+
+export default defineComponent(ProfileContent);
