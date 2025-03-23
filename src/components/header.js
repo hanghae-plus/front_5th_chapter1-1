17110586e62daa0b5c8167ff0df890ff4ba36a17
chloekim <chloe@perfitt.io>
2025-03-23 17:19:31 +0900

    feat: 로그인했고 안했고에 따라 header에 보여지는 값 구분될 수 있게 삼항연산처리

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,17 @@
-export const header = () => `
+import { Store } from "../store";
+
+const state = Store.getState();
+const isLogin = state.isLoggedIn;
+
+export const header = () => {
+  const loginHTML = isLogin
+    ? `
+        <li><a href="/profile" data-route-link class="text-gray-600">프로필</a></li>
+        <li><a href="#" data-route-link class="text-gray-600">로그아웃</a></li>
+        `
+    : `<li><a href="/login" data-route-link class="text-gray-600">로그인</a></li>`;
+
+  return `
     <header class="bg-blue-600 text-white p-4 sticky top-0">
         <h1 class="text-2xl font-bold">항해플러스</h1>
       </header>
@@ -6,8 +19,7 @@ export const header = () => `
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
           <li><a href="/" data-route-link class="text-blue-600">홈</a></li>
-          <li><a href="/profile" data-route-link class="text-gray-600">프로필</a></li>
-          <li><a href="#" data-route-link class="text-gray-600">로그아웃</a></li>
+      ${loginHTML}
         </ul>
-      </nav>
-`;
+      </nav>`;
+};
