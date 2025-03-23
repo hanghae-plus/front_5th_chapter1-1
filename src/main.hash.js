5745f1ec7427adab06da0add6b943c9871b3bdcc
KumJungMin <rmawjdals@naver.com>
2025-03-24 00:20:04 +0900

    fix: 해시 라우터 구동 오류 수정하기

--- src/main.hash.js
+++ src/main.hash.js
@@ -1,3 +1,10 @@
-import main from "./main.js";
+import { render } from "./helpers/render.js";
+import { createRouter } from "./core/router";
+import { routes, onBeforeEach } from "./router.js";
+import { Context } from "./helpers/context.js";
 
-main({ mode: "hash" });
+const root = render("#root");
+const router = createRouter(root, routes, { mode: "hash" });
+
+Context.provide("router", router);
+router.beforeEach(onBeforeEach).start();
--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import { createRouter } from "./core/router";
 import { routes, onBeforeEach } from "./router.js";
 import { Context } from "./helpers/context.js";
 
-export default function main({ mode = "history" } = {}) {
-  const root = render("#root");
-  const router = createRouter(root, routes, { mode });
+const root = render("#root");
+const router = createRouter(root, routes, { mode: "history" });
 
-  Context.provide("router", router);
-  router.beforeEach(onBeforeEach).start();
-}
-
-main();
+Context.provide("router", router);
+router.beforeEach(onBeforeEach).start();
