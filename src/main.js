dd450a7ec576810dc25fdfb2c4a84bd82e9aaa66
chloekim <chloe@perfitt.io>
2025-03-23 16:08:04 +0900

    feat: 앞뒤로 가기 가능하게 구현, 링크 클릭 시 페이지 전환되게 구현

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { renderRoute } from "./router";
+import { navigateTo, renderRoute } from "./router";
 
 // 맨 처음 렌더 시
 window.addEventListener("DOMContentLoaded", renderRoute);
+
+// 앞으로 가기 / 뒤로 가기 가능하게 하기
+window.addEventListener("popstate", renderRoute);
+
+// 링크 클릭 시 페이지 전환 SPA
+document.addEventListener("click", (event) => {
+  const link = event.target.closest("a[data-route-link]");
+  if (link) {
+    event.preventDefault();
+    navigateTo(link.getAttribute("href"));
+  }
+});
