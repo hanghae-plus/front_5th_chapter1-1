6db2b9acc0a05f5b401b1ef008a53a409e46b67e
KumJungMin <rmawjdals@naver.com>
2025-03-23 12:11:09 +0900

    refactor: 페이지를 역할 단위로 분리하기

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import MainPage from "./Main.js";
+import ErrorPage from "./Error.js";
+import LoginPage from "./Login.js";
+import ProfilePage from "./Profile.js";
+
+export { MainPage, ErrorPage, LoginPage, ProfilePage };
