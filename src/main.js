cbe26240133fd28dbf290cd63af8bba6b25fca9d
hanbeulYou <hanbeul.you@gmail.com>
2025-03-23 19:22:41 +0900

    feat: 라우터 분리

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import ProfilePage from "./pages/Profile";
 import LoginPage from "./pages/Login";
 import ErrorPage from "./pages/Error";
 
-document.body.innerHTML = `
-  ${MainPage()}
-  ${ProfilePage()}
-  ${LoginPage()}
-  ${ErrorPage()}
-`;
+const App = () => {
+  const path = window.location.pathname;
+  if (path === "/") return MainPage();
+  if (path === "/profile") return ProfilePage();
+  if (path === "/login") return LoginPage();
+  return ErrorPage();
+};
+
+const render = (page) => {
+  document.body.innerHTML = page;
+};
+
+render(App());
