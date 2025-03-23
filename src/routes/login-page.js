f4942022629b48596111ec02c0ac490b7292b533
anveloper <hitedin@gmail.com>
2025-03-23 18:10:45 +0900

    fix: 테스트 코드에 맞게 placeholder 수정

--- src/routes/login-page.js
+++ src/routes/login-page.js
@@ -18,7 +18,7 @@ const LoginPage = () => {
         <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
         <form>
           <div class="mb-4">
-            <input type="text" name="username" placeholder="이메일 또는 전화번호" class="w-full p-2 border rounded" required>
+            <input type="text" name="username" placeholder="사용자 이름" class="w-full p-2 border rounded" required>
           </div>
           <div class="mb-6">
             <input type="password" name="password" placeholder="비밀번호" class="w-full p-2 border rounded" required>
