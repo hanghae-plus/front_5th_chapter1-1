0ec2ea9d25667558bae68024c8f3d85fcfeb9cb4
Lee MinGi <mingi3442@gmail.com>
2025-03-24 06:29:30 +0900

    🚧 Update router

--- src/shared/libs/router.libs.js
+++ src/shared/libs/router.libs.js
@@ -4,7 +4,6 @@ class Router {
     window.addEventListener("popstate", this.handlePopState.bind(this));
     window.addEventListener("hashchange", this.handleHashChange.bind(this));
     this.isHashMode = window.location.pathname.includes("hash.html");
-    this.basePath = import.meta.env.BASE_URL || "/";
   }
 
   addRoute(path, handler) {
@@ -15,9 +14,8 @@ class Router {
     if (this.isHashMode) {
       window.location.hash = "#" + path;
     } else {
-      const fullPath = this.basePath + path;
-      history.pushState(null, "", fullPath);
-      this.handleRoute(fullPath);
+      history.pushState(null, "", path);
+      this.handleRoute(path);
     }
   }
 
@@ -35,17 +33,6 @@ class Router {
   }
 
   handleRoute(path) {
-    // * 배포 후 라우팅 처리
-    if (path.startsWith(this.basePath)) {
-      path = path.slice(this.basePath.length);
-    }
-    if (path === "") {
-      path = "/";
-    }
-
-    if (path.length > 1 && path.endsWith("/")) {
-      path = path.slice(0, -1);
-    }
     const handler = this.routes[path] || this.routes["*"];
     handler?.();
   }
