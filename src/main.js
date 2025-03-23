e545fb19f71d1f7480929f9893d2d7c42076ad49
hanbeulYou <hanbeul.you@gmail.com>
2025-03-23 21:20:22 +0900

    fix: Router 로직 수정

--- src/main.js
+++ src/main.js
@@ -3,16 +3,54 @@ import ProfilePage from "./pages/Profile";
 import LoginPage from "./pages/Login";
 import ErrorPage from "./pages/Error";
 
-const App = () => {
-  const path = window.location.pathname;
-  if (path === "/") return MainPage();
-  if (path === "/profile") return ProfilePage();
-  if (path === "/login") return LoginPage();
-  return ErrorPage();
+class Router {
+  constructor(routes) {
+    this.routes = routes;
+    this.currentPath = window.location.pathname;
+
+    window.addEventListener("popstate", () => {
+      this.currentPath = window.location.pathname;
+      this.render();
+    });
+  }
+
+  navigate(to) {
+    window.history.pushState({}, "", to);
+    this.currentPath = to;
+    this.render();
+  }
+
+  render() {
+    const page = this.routes[this.currentPath]
+      ? this.routes[this.currentPath]()
+      : ErrorPage();
+    document.body.innerHTML = page;
+  }
+}
+
+const isLoggedIn = () => {
+  const user = localStorage.getItem("user");
+  return !!user;
 };
 
-const render = (page) => {
-  document.body.innerHTML = page;
+const routes = {
+  "/": MainPage,
+  "/profile": () => {
+    if (!isLoggedIn()) {
+      router.navigate("/login");
+      return LoginPage();
+    }
+    return ProfilePage();
+  },
+  "/login": LoginPage,
 };
 
-render(App());
+const router = new Router(routes);
+router.render();
+
+document.addEventListener("click", (e) => {
+  if (e.target.matches("[data-link]")) {
+    e.preventDefault();
+    router.navigate(e.target.getAttribute("href"));
+  }
+});
