9cdf13de832e54fc62040900284e6c60a2908b85
DohyungKim <ehgud456456@naver.com>
2025-03-23 19:53:50 +0900

    refactor: 상수들을 별도 파일로 분리

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const CONST = {
+  userDB: "user-db",
+  loginForm: {
+    formId: "login-form",
+    field: {
+      email: "email",
+      password: "password",
+    },
+  },
+  profileForm: {
+    formId: "profile-form",
+    field: {
+      username: "username",
+      email: "email",
+      bio: "bio",
+    },
+  },
+  pathname: {
+    main: "/",
+    login: "/login",
+    profile: "/profile",
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,26 +1,4 @@
-const CONST = {
-  userDB: "user-db",
-  loginForm: {
-    formId: "login-form",
-    field: {
-      email: "email",
-      password: "password",
-    },
-  },
-  profileForm: {
-    formId: "profile-form",
-    field: {
-      username: "username",
-      email: "email",
-      bio: "bio",
-    },
-  },
-  pathname: {
-    main: "/",
-    login: "/login",
-    profile: "/profile",
-  },
-};
+import { CONST } from "./constants";
 
 const savedUserDB = localStorage.getItem(CONST.userDB) || "{}";
 const userDB = JSON.parse(savedUserDB);
