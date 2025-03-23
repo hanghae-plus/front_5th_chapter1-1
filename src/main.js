411de652e7a8fef9c7bf17733e6a8b207c4e669d
chloekim <chloe@perfitt.io>
2025-03-23 14:57:47 +0900

    footer 공통 컴포넌트로 추가

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { footer } from "./components/footer";
 import { header } from "./components/header";
 import { userData } from "./data/user";
 
@@ -33,14 +34,11 @@ const MainPage = () => {
           <textarea class="w-full p-2 border rounded" placeholder="무슨 생각을 하고 계신가요?"></textarea>
           <button class="mt-2 bg-blue-600 text-white px-4 py-2 rounded">게시</button>
         </div>
-
         <div class="space-y-4">
         ${userDataHTML}
         </div>
       </main>
-      <footer class="bg-gray-200 p-4 text-center">
-        <p>&copy; 2024 항해플러스. All rights reserved.</p>
-      </footer>
+    ${footer}
     </div>
   </div>`;
 };
@@ -148,10 +146,7 @@ const ProfilePage = () => `
             </form>
           </div>
         </main>
-
-        <footer class="bg-gray-200 p-4 text-center">
-          <p>&copy; 2024 항해플러스. All rights reserved.</p>
-        </footer>
+        ${footer}
       </div>
     </div>
   </div>
