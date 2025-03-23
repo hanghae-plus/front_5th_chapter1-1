a2ccfa55a4e8a185a8785e5d661052e9f4f60e04
KumJungMin <rmawjdals@naver.com>
2025-03-23 13:22:43 +0900

    feat: 네비게이션 버튼에 활성화 스타일 적용하기

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,8 @@
 export default function Header() {
+  function isActive(path) {
+    const pathname = window.location.pathname;
+    return pathname === path ? "text-blue-600" : "text-gray-600";
+  }
   return `
     <header class="bg-blue-600 text-white p-4 sticky top-0">
       <h1 class="text-2xl font-bold">항해플러스</h1>
@@ -6,8 +10,8 @@ export default function Header() {
 
     <nav class="bg-white shadow-md p-2 sticky top-14">
       <ul class="flex justify-around">
-        <li><a href="/" class="text-blue-600">홈</a></li>
-        <li><a href="/profile" class="text-gray-600">프로필</a></li>
+        <li><a href="/" class="${isActive("/")}">홈</a></li>
+        <li><a href="/profile" class="${isActive("/profile")}">프로필</a></li>
         <li><a href="#" class="text-gray-600">로그아웃</a></li>
       </ul>
     </nav>
