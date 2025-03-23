b1bea99ef93dc6d017e0ca7c07b03769429e0d5a
hanbeulYou <hanbeul.you@gmail.com>
2025-03-23 22:55:42 +0900

    fix: 잘못 작성된 placeHolder 변경

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -6,7 +6,7 @@ const LoginForm = () => {
   return `
     <form id="login-form">
       <div class="mb-4">
-        <input id="username" type="text" placeholder="이메일 또는 전화번호" class="w-full p-2 border rounded">
+        <input id="username" type="text" placeholder="사용자 이름" class="w-full p-2 border rounded">
       </div>
       <div class="mb-6">
         <input id="password" type="password" placeholder="비밀번호" class="w-full p-2 border rounded">
