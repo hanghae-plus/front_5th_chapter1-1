6137f35201524f321d111fdd694e07fc628ca067
KumJungMin <rmawjdals@naver.com>
2025-03-23 14:47:10 +0900

    feat: 로그인 정보를 관리하는 스토어 추가하기

---
+++ src/stores/user.js
@@ -0,0 +1,24 @@
+class UserStore {
+  #userInfo = null;
+  #localKey = "userInfo:v1";
+  static instance;
+
+  constructor() {
+    if (UserStore.instance) return UserStore.instance;
+    UserStore.instance = this;
+    this.#userInfo = JSON.parse(localStorage.getItem(this.#localKey) || "null");
+  }
+
+  setUserInfo(info) {
+    this.#userInfo = info;
+    localStorage.setItem(this.#localKey, JSON.stringify(info));
+  }
+
+  get userInfo() {
+    return this.#userInfo;
+  }
+}
+
+export function useUserStore() {
+  return new UserStore();
+}
