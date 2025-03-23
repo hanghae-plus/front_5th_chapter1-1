db9f09aa0bafd33a65354353e992183679210a5a
sungchan.kim <sungchan.kim@datastreams.co.kr>
2025-03-23 23:46:37 +0900

    Feat: 1) 라우팅 구현 -  임의 state 생성 - history API 서용한 라우팅 구현 - 라우팅에 따라 page 렌더링

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,18 @@ export default () => {
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" class="text-blue-600">홈</a></li>
-          <li><a href="/profile" class="text-gray-600">프로필</a></li>
-          <li><a href="#" class="text-gray-600">로그아웃</a></li>
+          ${
+            window.state.loggedIn
+              ? `
+              <li><a href="/" class="${location.pathname === "/" ? "text-blue-600" : "text-gray-600"}">홈</a></li>
+              <li><a href="/profile" class="${location.pathname === "/profile" ? "text-blue-600" : "text-gray-600"}">프로필</a></li>
+              <li><a href="#" class="text-gray-600">로그아웃</a></li>
+          `
+              : `
+              <li><a href="/" class="${location.pathname === "/" ? "text-blue-600" : "text-gray-600"}">홈</a></li>
+              <li><a href="/profile" class="${location.pathname === "/profile" ? "text-blue-600" : "text-gray-600"}">프로필</a></li>
+          `
+          }
         </ul>
       </nav>
     `;
--- src/main.js
+++ src/main.js
@@ -3,9 +3,47 @@ import ErrorPage from "./components/ErrorPage";
 import LoginPage from "./components/LoginPage";
 import ProfilePage from "./components/ProfilePage";
 
-document.body.innerHTML = `
-  ${MainPage()}
-  ${ProfilePage()}
-  ${LoginPage()}
-  ${ErrorPage()}
-`;
+window.state = {
+  loggedIn: false,
+};
+
+const Page = () => {
+  switch (location.pathname) {
+    case "/":
+      return MainPage();
+    case "/login":
+      return LoginPage();
+    case "/profile":
+      if (window.state.loggedIn) {
+        return ProfilePage();
+      } else {
+        history.pushState({ path: "/login" }, "", "/login");
+        return LoginPage();
+      }
+    default:
+      return ErrorPage();
+  }
+};
+
+const render = () => {
+  document.querySelector("#root").innerHTML = Page();
+};
+
+const router = () => {
+  render();
+
+  document.querySelectorAll("a").forEach((el) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      const newPathname = e.target.href.replace(location.origin, "");
+      history.pushState({ path: newPathname }, "", newPathname);
+      router();
+    });
+  });
+};
+
+window.addEventListener("popstate", () => {
+  router();
+});
+
+router();
