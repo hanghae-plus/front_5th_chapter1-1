59405d6c310aebc897774778a166ef78eff093dc
KumJungMin <rmawjdals@naver.com>
2025-03-23 19:48:24 +0900

    feat: router 주입하기

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { render } from "./helpers/render.js";
 import { createRouter } from "./helpers/router.js";
 import { routes, onBeforeEach } from "./router.js";
+import { Context } from "./helpers/context.js";
 
-const root = render("#app");
+const root = render("#root");
 
 const router = createRouter(root, routes);
+
+Context.provide("router", router);
+
 router.beforeEach(onBeforeEach).start();
