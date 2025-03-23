19f6305fb2a1c81b8c608857ae860e99d1f43963
KumJungMin <rmawjdals@naver.com>
2025-03-23 19:48:44 +0900

    feat: 로그아웃 클릭시 데이터 초기화하기

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,43 @@
 import { useUserStore } from "../stores/user.js";
+import { defineComponent } from "../helpers/component";
 
-export default function Header() {
-  const userStore = useUserStore();
+const HeaderContent = {
+  name: "Header",
+  template: () => {
+    const userStore = useUserStore();
 
-  function isActive(path) {
-    const pathname = window.location.pathname;
-    return pathname === path ? "text-blue-600" : "text-gray-600";
-  }
-  return `
-    <header class="bg-blue-600 text-white p-4 sticky top-0">
-      <h1 class="text-2xl font-bold">항해플러스</h1>
-    </header>
+    function isActive(path) {
+      const pathname = window.location.pathname;
+      return pathname === path ? "text-blue-600" : "text-gray-600";
+    }
 
-    <nav class="bg-white shadow-md p-2 sticky top-14">
-      <ul class="flex justify-around">
-        <li><a href="/" class="${isActive("/")}">홈</a></li>
-        <li><a href="/profile" class="${isActive("/profile")}">프로필</a></li>
-        ${
-          userStore.isAuthenticated
-            ? '<li><a href="#" class="text-gray-600">로그아웃</a></li>'
-            : `<li><a href="/login" class="${isActive("/login")}">로그인</a></li>`
-        }
-      </ul>
-    </nav>
+    return `
+      <header class="bg-blue-600 text-white p-4 sticky top-0">
+        <h1 class="text-2xl font-bold">항해플러스</h1>
+      </header>
+
+      <nav class="bg-white shadow-md p-2 sticky top-14">
+        <ul class="flex justify-around" id="menu">
+          <li><a href="/" class="${isActive("/")}">홈</a></li>
+          <li><a href="/profile" class="${isActive("/profile")}">프로필</a></li>
+          ${
+            userStore.isAuthenticated
+              ? '<li><a href="/login" id="logout" class="text-gray-600">로그아웃</a></li>'
+              : `<li><a href="/login" class="${isActive("/login")}">로그인</a></li>`
+          }
+        </ul>
+      </nav>
     `;
-}
+  },
+  domEvent: () => {
+    const userStore = useUserStore();
+    const logoutButton = document.querySelector("#logout");
+
+    logoutButton?.addEventListener("click", (event) => {
+      event.preventDefault();
+      userStore.removeUserInfo();
+    });
+  },
+};
+
+export default defineComponent(HeaderContent);
