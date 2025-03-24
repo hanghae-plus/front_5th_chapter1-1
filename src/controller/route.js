2b799aa190fb8fd8838abaac0fa981f83e1b6663
chipmunkcol <jack95711@gmail.com>
2025-03-24 22:54:31 +0900

    router controller

---
+++ src/controller/route.js
@@ -0,0 +1,39 @@
+class Router {
+  constructor() {
+    this.routes = {};
+    window.addEventListener("popstate", this.handlePopState.bind(this));
+  }
+
+  addRoute(path, handler) {
+    this.routes[path] = handler;
+  }
+
+  navigateTo(path) {
+    history.pushState(null, "", path);
+    this.handleRoute(path);
+  }
+
+  handlePopState() {
+    this.handleRoute(window.location.pathname);
+  }
+
+  handleRoute(path) {
+    const handler = this.routes[path];
+    if (handler) {
+      handler();
+    } else {
+      console.log("404 Not Found");
+    }
+  }
+}
+
+export const router = new Router();
+// router.addRoute('/', () => console.log('Home Page'));
+// router.addRoute('/about', () => console.log('About Page'));
+
+// document.querySelector('nav').addEventListener('click', (e) => {
+//   if (e.target.tagName === 'A') {
+//       e.preventDefault();
+//       router.navigateTo(e.target.pathname);
+//   }
+// });
