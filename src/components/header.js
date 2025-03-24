97abff1b79f6205c8e71a2e760cf4127840f230d
chloekim <chloe@perfitt.io>
2025-03-24 20:26:29 +0900

    fix:github페이지 배포위해 base url 추가

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,15 @@ export const header = () => {
   const isLogin = Store.logIn();
 
   const currentPath = window.location.pathname;
+  const isProd = location.hostname.includes("github.io");
+  const BASE_PATH = isProd ? "/front-5th-chapter1-1" : "";
 
   const loginHTML = isLogin
     ? `
-        <li><a href="/profile" data-route-link class="${currentPath === "/profile" ? "text-blue-600" : "text-gray-600"}">프로필</a></li>
+        <li><a href="/profile" data-route-link class="${currentPath === `${BASE_PATH}/profile` ? "text-blue-600" : "text-gray-600"}">프로필</a></li>
         <li><a href="/login" id="logout" data-route-link class="text-gray-600">로그아웃</a></li>
         `
-    : `<li><a href="/login" data-route-link class="${currentPath === "/login" ? "text-blue-600" : "text-gray-600"}">로그인</a></li>`;
+    : `<li><a href="/login" data-route-link class="${currentPath === `${BASE_PATH}/login` ? "text-blue-600" : "text-gray-600"}">로그인</a></li>`;
 
   return `
     <header class="bg-blue-600 text-white p-4 sticky top-0">
@@ -19,7 +21,7 @@ export const header = () => {
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" data-route-link class=${currentPath === "/" ? "text-blue-600" : "text-gray-600"}>홈</a></li>
+          <li><a href="/" data-route-link class=${currentPath === `${BASE_PATH}/` ? "text-blue-600" : "text-gray-600"}>홈</a></li>
       ${loginHTML}
         </ul>
       </nav>`;
--- src/router.js
+++ src/router.js
@@ -4,10 +4,13 @@ import { NotFoundPage } from "./pages/notFound";
 import { ProfilePage, updateProfile } from "./pages/profile";
 import { Store } from "./store";
 
+const isProd = location.hostname.includes("github.io");
+const BASE_PATH = isProd ? "/front-5th-chapter1-1" : "";
+
 const routes = {
-  "/": MainPage,
-  "/login": LoginPage,
-  "/profile": ProfilePage,
+  [`${BASE_PATH}/`]: MainPage,
+  [`${BASE_PATH}/login`]: LoginPage,
+  [`${BASE_PATH}/profile`]: ProfilePage,
 };
 
 export function renderRoute() {
@@ -23,9 +26,9 @@ export function renderRoute() {
   }
 
   //1. 로그인 안된 상태에서 프로필 페이지 접근 시 -> 로그인페이지로 리다이렉트
-  if (path === "/profile" && !isLogin) {
-    history.replaceState(null, "", "/login");
-    root.innerHTML = routes["/login"]();
+  if (path === `${BASE_PATH}/profile` && !isLogin) {
+    history.replaceState(null, "", `${BASE_PATH}/login`);
+    root.innerHTML = routes[`${BASE_PATH}/login`]();
     return;
   }
 
@@ -34,12 +37,12 @@ export function renderRoute() {
   root.innerHTML = Page();
 
   // 3.로그인 폼 제출관련
-  if (path === "/login") {
+  if (path === `${BASE_PATH}/login`) {
     setUpLoginForm();
     return;
   }
   // 4. 프로필 업데이트 관련
-  if (path === "/profile" && isLogin) {
+  if (path === `${BASE_PATH}/profile` && isLogin) {
     updateProfile();
   }
 
@@ -50,8 +53,8 @@ export function renderRoute() {
       e.preventDefault();
       Store.logout();
 
-      history.replaceState(null, "", "/login");
-      root.innerHTML = routes["/login"]();
+      history.replaceState(null, "", `${BASE_PATH}/login`);
+      root.innerHTML = routes[`${BASE_PATH}/login`]();
     });
   }
 }
