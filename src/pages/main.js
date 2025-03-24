e9d1a0c0ca551e55ce813ddb4045fb36fa97e59e
Doh Kim <ehgud456456@naver.com>
2025-03-24 15:40:15 +0900

    fix: nav component style 변경

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,7 +8,7 @@ export const MainPage = (container) => {
     <div class="bg-gray-100 min-h-screen flex justify-center">
       <div class="max-w-md w-full">
         ${Header()}
-        <nav></nav>
+        <nav class="bg-white shadow-md p-2 sticky top-14"></nav>
         <main class="p-4">
           <div class="mb-4 bg-white rounded-lg shadow p-4">
             <textarea class="w-full p-2 border rounded" placeholder="무슨 생각을 하고 계신가요?"></textarea>
