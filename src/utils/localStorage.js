1ad7402ce95da7654604cb0b0de963e8c83f963a
chaaerim <cofla411@ewhain.net>
2025-03-24 13:50:58 +0900

    feat: add localStorage utils

---
+++ src/utils/localStorage.js
@@ -0,0 +1,17 @@
+export const getData = (key, defaultValue) => {
+  try {
+    const todo = window.localStorage.getItem(key);
+    return todo ? JSON.parse(todo) : defaultValue;
+  } catch (e) {
+    console.log(e);
+    return defaultValue;
+  }
+};
+
+export const setData = (key, value) => {
+  try {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    console.log(e);
+  }
+};
