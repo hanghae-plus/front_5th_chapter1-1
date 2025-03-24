9a24e656b5234503ff1eda49814e957984a4884b
Lee MinGi <mingi3442@gmail.com>
2025-03-25 01:06:19 +0900

    🚧 Update router for build test

--- src/shared/libs/router.libs.js
+++ src/shared/libs/router.libs.js
@@ -1,16 +1,9 @@
 class Router {
-  constructor() {
+  constructor(basePath = "") {
     this.routes = {};
-    this.basePath = this.getBasePath();
+    this.basePath = basePath;
     window.addEventListener("popstate", this.handlePopState.bind(this));
     window.addEventListener("hashchange", this.handleHashChange.bind(this));
-  }
-
-  getBasePath() {
-    //
-    return window.location.hostname.includes("github.io")
-      ? "/front_5th_chapter1-1"
-      : "";
   }
 
   get isHashMode() {
@@ -21,28 +14,23 @@ class Router {
   }
 
   addRoute(path, handler) {
-    const normalizedPath = path.startsWith("/") ? path : `/${path}`;
-    this.routes[normalizedPath] = handler;
+    this.routes[path] = handler;
   }
 
   navigateTo(path) {
-    const normalizedPath = path.startsWith("/") ? path : `/${path}`;
-
     if (this.isHashMode) {
-      window.location.hash = normalizedPath;
-      const routePath = normalizedPath || "/";
+      window.location.hash = path;
+      const routePath = path || "/";
       this.handleRoute(routePath);
     } else {
-      const fullPath = `${this.basePath}${normalizedPath}`;
-      history.pushState(null, "", fullPath);
-      this.handleRoute(normalizedPath);
+      history.pushState(null, "", `${this.basePath}${path}`);
+      this.handleRoute(path);
     }
   }
 
   handlePopState() {
     if (!this.isHashMode) {
-      const path = window.location.pathname.replace(this.basePath, "") || "/";
-      this.handleRoute(path);
+      this.handleRoute(window.location.pathname);
     }
   }
 
@@ -60,16 +48,7 @@ class Router {
       document.body.appendChild(root);
     }
 
-    const normalizedPath = path.startsWith("/") ? path : `/${path}`;
-
-    let routePath = normalizedPath;
-    if (routePath.startsWith(this.basePath)) {
-      routePath = routePath.replace(this.basePath, "") || "/";
-    }
-
-    if (routePath === "") routePath = "/";
-
-    const handler = this.routes[routePath] || this.routes["*"];
+    const handler = this.routes[path] || this.routes["*"];
 
     if (handler) {
       handler();
@@ -78,4 +57,6 @@ class Router {
 }
 
 // ? singleton pattern을 위한 export
-export const router = new Router();
+export const router = new Router(
+  window.location.hostname.includes("github.io") ? "/front_5th_chapter1-1" : "",
+);
