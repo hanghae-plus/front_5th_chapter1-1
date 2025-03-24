46c9ed93a09bc5751488a1154b3ba39e5e90511b
3jun <kk3june@gmail.com>
2025-03-24 22:07:20 +0900

    feat. 컴포넌트 분리

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+function Header(path) {
+  return `
+		<header class="bg-blue-600 text-white p-4 sticky top-0">
+			<h1 class="text-2xl font-bold">항해플러스</h1>
+		</header>
+
+		<nav class="bg-white shadow-md p-2 sticky top-14">
+			<ul class="flex justify-around">
+				<li><a href="/" class="${path === "/" ? "text-blue-600" : "text-gray-600"}">홈</a></li>
+				<li><a href="/profile" class="${path === "/profile" ? "text-blue-600" : "text-gray-600"}">프로필</a></li>
+				<li><a href="#" class="text-gray-600">로그아웃</a></li>
+			</ul>
+		</nav>
+		`;
+}
+
+export default Header;
