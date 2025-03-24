975df74306e897432ed1a0bc940834d9fa829c7c
User850413 <csyou1228@gmail.com>
2025-03-24 16:33:49 +0900

    03/24 :recycle:: header 컴포넌트 분리

--- src/components/header.js
+++ src/components/header.js
@@ -1,2 +1,24 @@
-const header = ``;
+const header = ({ isLoggedIn }) => {
+  return `
+    <header class="bg-blue-600 text-white p-4 sticky top-0">
+      <h1 class="text-2xl font-bold">항해플러스</h1>
+    </header>
+
+    <nav class="bg-white shadow-md p-2 sticky top-14">
+      <ul class="flex justify-around">
+      ${
+        isLoggedIn
+          ? `
+           <li><a href="/" class="text-blue-600">홈</a></li>
+           <li><a id="btn-profile" href="/profile" class="text-gray-600">프로필</a></li>
+           <li><a href="#" class="text-gray-600">로그아웃</a></li>`
+          : `
+            <li><a href="/" class="text-blue-600">홈</a></li>
+            <li><a id="btn-login" href="/login" class="text-gray-600">로그인</a></li>
+            `
+      }
+        
+      </ul>
+    </nav>`;
+};
 export default header;
