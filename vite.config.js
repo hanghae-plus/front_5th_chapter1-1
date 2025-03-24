d4a479303a8f6e6d416c5f14c1ba1bb945937993
Doh Kim <ehgud456456@naver.com>
2025-03-24 18:30:40 +0900

    ci: gh-pages 배포를 위해 라이브러리 설치

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter1-1/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.hash.html",
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
