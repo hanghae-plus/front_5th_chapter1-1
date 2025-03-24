f5e0d94196d66e771a5ce3d28c96925eb276c813
Doh Kim <ehgud456456@naver.com>
2025-03-24 16:04:12 +0900

    feat: nav에 현재 path 강조

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,10 +2,19 @@ import { CONST } from "../data/constants";
 import { state } from "../data/state";
 
 export const NavContent = () => {
+  const getActiveClassName = (path) => {
+    const isCurrentPath = window.location.pathname === path;
+    return isCurrentPath ? "text-blue-600 font-bold" : "text-gray-600";
+  };
+
   return `
     <ul class="flex justify-around">
-      <li><a href="/"  class="text-blue-600">홈</a></li>
-      ${state.loggedInUser ? '<li><a href="/profile" class="text-gray-600">프로필</a></li>' : ""}
+      <li><a href="${CONST.pathname.main}" class="${getActiveClassName(CONST.pathname.main)}">홈</a></li>
+      ${
+        state.loggedInUser
+          ? `<li><a href="${CONST.pathname.profile}" class="${getActiveClassName(CONST.pathname.profile)}">프로필</a></li>`
+          : ""
+      }
       ${
         state.loggedInUser
           ? '<li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>'
