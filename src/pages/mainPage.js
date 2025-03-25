382a231896111b4dbeb635e8a03d172588fc9871
devchaeyoung <chaemaa00@gmail.com>
2025-03-26 02:26:12 +0900

    design: 불필요한 요소 제거

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -25,15 +25,14 @@ export default function MainPage({ loginState, posts }) {
           <main class="p-4">
            ${
              loginState
-               ? `<div class="mb-4 bg-white rounded-lg shadow p-4">
+               ? /* html */ `<div class="mb-4 bg-white rounded-lg shadow p-4">
               <textarea class="w-full p-2 border rounded" placeholder="무슨 생각을 하고 계신가요?"></textarea>
               <button class="mt-2 bg-blue-600 text-white px-4 py-2 rounded">게시</button>
             </div>`
                : ""
-           };
-    
+           }    
             <div class="space-y-4">
-             ${posts.map(Post).join()}
+             ${posts.map(Post).join("")}
             </div>
           </main>
     
