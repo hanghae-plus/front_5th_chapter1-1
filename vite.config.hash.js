9c6dc770a535484350742c206891570a75d9222f
anveloper <hitedin@gmail.com>
2025-03-25 22:00:51 +0900

    test: hash파일로도 gh-page동작할 수 있는 지 테스트

---
+++ vite.config.hash.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  base: "/front_5th_chapter1-1/",
+  build: {
+    rollupOptions: {
+      input: "index.hash.html",
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+  },
+});
