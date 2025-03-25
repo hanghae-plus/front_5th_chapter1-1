9dcafbf543d292f78881247d049ea44e21940b99
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2025-03-26 02:00:59 +0900

    feat: 초기 설정과 이벤트 리스너 관리 추가 뒤로가기 앞으로 가기 시 경로 변경 처리, 링크 클릭시에 새로고침 방지 추가

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import { MainPage } from "./pages/homepage";
-import { ErrorPage } from "./pages/notFoundPage";
-import { ProfilePage } from "./pages/profilePage";
-import { LoginPage } from "./pages/loginPage";
+import { navigateTo, handlePopState } from "./update/update";
 
-document.body.innerHTML = `
-  ${MainPage()}
-  ${ProfilePage()}
-  ${LoginPage()}
-  ${ErrorPage()}
-`;
+// 페이지 로드 시 초기 경로 설정
+window.addEventListener("load", () => {
+  // 페이지 로드 시 경로에 맞는 렌더링
+  const initialPath = window.location.pathname;
+  navigateTo(initialPath);
+});
+
+// 뒤로가기, 앞으로가기 시 경로 변경 처리
+window.addEventListener("popstate", handlePopState);
+
+// 링크 클릭 이벤트 처리
+document.addEventListener("click", (event) => {
+  if (event.target.tagName === "A" && event.target.href) {
+    event.preventDefault(); // 기본 링크 동작 방지
+    const path = new URL(event.target.href).pathname;
+    navigateTo(path);
+  }
+});
