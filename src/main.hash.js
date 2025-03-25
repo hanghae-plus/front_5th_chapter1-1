9c31401ea69f20f8b12a0e7b44aafb5e5dcacbc9
anveloper <hitedin@gmail.com>
2025-03-25 21:39:07 +0900

    feat: 테스트 통과완료

--- src/main.hash.js
+++ src/main.hash.js
@@ -1,1 +1,91 @@
-import "./main.js";
+import { CUSTOM_EVENT, ELEMENT_ID, ROUTES } from "./config/index.js";
+import ErrorPage from "./routes/error-page.js";
+import LoginPage from "./routes/login-page.js";
+import MainPage from "./routes/main-page.js";
+import ProfilePage from "./routes/profile-page.js";
+import store from "./store/index.js";
+
+const PREFIX = "/front_5th_chapter1-1";
+const HASH_TEXT = "#";
+
+const handleLink = (e) => {
+  e.preventDefault();
+  let url = e.target.getAttribute("href");
+  if (url === ROUTES.LOGOUT) {
+    localStorage.removeItem("user");
+    url = ROUTES.MAIN;
+  } else if (url === ROUTES.LOGIN) {
+    if (store.get("username")) url = ROUTES.MAIN;
+  }
+  const config = { detail: { url }, bubbles: true, cancelable: true };
+  document.dispatchEvent(new CustomEvent(CUSTOM_EVENT.PAGE_PUSH, config));
+};
+
+const eventListener = () => {
+  document.querySelectorAll("a").forEach((el) => {
+    el.addEventListener("click", handleLink);
+  });
+};
+
+const URL_MAP = {
+  [ROUTES.PROFILE]: ProfilePage,
+  [ROUTES.LOGIN]: LoginPage,
+  [ROUTES.MAIN]: MainPage,
+  [ROUTES.ERROR]: ErrorPage,
+};
+
+const navigate = (e) => {
+  e.preventDefault();
+  let url = e.detail?.url || location.hash || ROUTES.MAIN;
+  const isLogin = store.isLogon();
+  url = url.replace(HASH_TEXT, "");
+  if (!isLogin && url === ROUTES.PROFILE) url = ROUTES.LOGIN;
+  if (!!isLogin && url === ROUTES.LOGIN) url = ROUTES.MAIN;
+  if (!url.includes(HASH_TEXT)) url = HASH_TEXT + url;
+  if (url.includes(PREFIX)) url = url.replace(PREFIX, "");
+  location.hash = url;
+  render(url);
+};
+
+const render = (url) => {
+  const key = url.replace(PREFIX, "").replace(HASH_TEXT, "");
+  const page = (URL_MAP[key] || ErrorPage)();
+  const root = document.querySelector("#root");
+  root.innerHTML = page.template;
+  eventListener();
+};
+
+// 로그인에서도 커스텀 이벤트로 페이지 이동
+document.addEventListener(CUSTOM_EVENT.PAGE_PUSH, navigate);
+
+document.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (e.target.id === ELEMENT_ID.LOGIN_FORM) {
+    const formData = new FormData(e.target);
+    const username = formData.get("username") || "";
+    const data = { username, email: "", bio: "" };
+    localStorage.setItem("user", JSON.stringify(data));
+    store.set(data);
+    const url = ROUTES.MAIN;
+    const config = { detail: { url }, bubbles: true, cancelable: true };
+    document.dispatchEvent(new CustomEvent(CUSTOM_EVENT.PAGE_PUSH, config));
+  } else if (e.target.id === ELEMENT_ID.PROFILE_FORM) {
+    const formData = new FormData(e.target);
+    const data = {
+      username: formData.get("username") || "",
+      email: formData.get("email") || "",
+      bio: formData.get("bio") || "",
+    };
+    localStorage.setItem("user", JSON.stringify(data));
+    store.set(data);
+    const url = ROUTES.PROFILE;
+    const config = { detail: { url }, bubbles: true, cancelable: true };
+    document.dispatchEvent(new CustomEvent(CUSTOM_EVENT.PAGE_PUSH, config));
+  }
+});
+
+// 기본 페이지 이동, DOM 로드 이벤트 변경..
+window.addEventListener("storage", store.sync());
+window.addEventListener("hashchange", navigate);
+window.addEventListener("popstate", navigate);
+window.addEventListener("DOMContentLoaded", navigate);
