e950ba6e91667032dce743407f67fbf54963859e
minhee <jmh3699@naver.com>
2025-03-26 09:54:04 +0900

    feat: path에 맞게 nav 메뉴 스타일 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,11 @@ export const Header = () => {
     "top-0",
   );
 
+  const currPath = window.location.pathname;
+  const makeLiClass = (href) => {
+    return href === currPath ? "text-blue-600 font-bold" : "text-gray-600";
+  };
+
   const render = () => {
     headerElem.innerHTML = `
       <h1 class="text-2xl font-bold">항해플러스</h1>
@@ -18,13 +23,13 @@ export const Header = () => {
           ${
             state.isLoggedIn
               ? `
-              <li><a href="/" class="text-blue-600">홈</a></li>
-              <li><a href="/profile" class="text-gray-600">프로필</a></li>
+              <li><a href="/" class="${makeLiClass(`/`)}">홈</a></li>
+              <li><a href="/profile" class="${makeLiClass(`/profile`)}">프로필</a></li>
               <li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>
               `
               : `
-              <li><a href="/" class="text-blue-600">홈</a></li>
-              <li><a href="/login" class="text-gray-600">로그인</a></li>
+              <li><a href="/" class="${makeLiClass(`/`)}">홈</a></li>
+              <li><a href="/login" class="${makeLiClass(`/login`)}">로그인</a></li>
               `
           }
           </ul>
