ae997e53440825b2f4f5fc9c791ccaf027274442
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2025-03-27 21:22:25 +0900

    feat: 초기상태와 액션타입 정의

--- src/model/model.js
+++ src/model/model.js
@@ -1,12 +1,14 @@
-export const model = {
+export const initialState = {
   path: "/",
   isLoggedIn: false,
+  user: null,
+  error: null,
+};
 
-  setPath(newPath) {
-    this.path = newPath;
-  },
-
-  setLoginStatus(status) {
-    this.isLoggedIn = status;
-  },
+export const ActionTypes = {
+  SET_PATH: "SET_PATH",
+  LOGIN: "LOGIN",
+  LOGOUT: "LOGOUT",
+  UPDATE_PROFILE: "UPDATE_PROFILE",
+  SET_ERROR: "SET_ERROR",
 };
