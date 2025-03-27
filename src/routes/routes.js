a00a9bd8214de523f638db2d248846621196f3d8
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-03-28 03:12:03 +0900

    refactor

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import { BASE_PATH } from "../constants";
 import { ErrorPage, LoginPage, MainPage, ProfilePage } from "../pages";
+import { handleLogin, updateProfile } from "../services/auth";
 import { userStore } from "../store/store";
 
 const routes = {
@@ -11,26 +12,6 @@ const routes = {
 const renderPageWithHistory = (path) => {
   history.pushState(null, null, path);
   render();
-};
-
-const handleLogin = (elements) => {
-  const username = elements.username.value;
-
-  if (!username || !username.trim()) {
-    return window.alert("이름을 입력해주세요.");
-  }
-
-  userStore.setUserInfo({ username, email: "", bio: "" });
-  renderPageWithHistory(`${BASE_PATH}/profile`);
-};
-
-const updateProfile = (elements) => {
-  const username = elements.username.value;
-  const email = elements.email.value;
-  const bio = elements.bio.value;
-
-  userStore.setUserInfo({ username, email, bio });
-  alert("프로필이 업데이트 되었어요!");
 };
 
 export const render = () => {
@@ -76,6 +57,7 @@ export const render = () => {
 
       if (e.target.id === "login-form") {
         handleLogin(e.target.elements);
+        renderPageWithHistory(`${BASE_PATH}/profile`);
       }
 
       if (e.target.id === "profile-form") {
@@ -88,4 +70,4 @@ export const render = () => {
   );
 };
 
-window.addEventListener("popstate", () => render());
+window.addEventListener("popstate", render);
