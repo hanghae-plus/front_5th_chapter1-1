ce6b7ed9bc6db1d5c279a600449584e1b6c7a077
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2025-03-28 03:08:49 +0900

    test_report
    change header

--- src/main.js
+++ src/main.js
@@ -464,9 +464,7 @@ const ProfilePage = () => {
   return /*html*/ `
   <div class="bg-gray-100 min-h-screen flex justify-center">
     <div class="max-w-md w-full">
-      <header class="bg-blue-600 text-white p-4 sticky top-0">
-        <h1 class="text-2xl font-bold">항해플러스</h1>
-      </header>
+      ${Header({ loggedIn: state.loggedIn, username: state.username })}
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
