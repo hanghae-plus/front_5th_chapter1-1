147a87e526ff923970ecde18a2c50ab9a4d2e066
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2025-03-28 06:25:07 +0900

    fix: 초기 상태 저장 방지 조건추가

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -8,14 +8,20 @@ export const userContext = createContext({
 });
 
 function syncLocalStorage(state) {
-  if (state.isLoggedIn && state.user) {
-    localStorage.setItem("user", JSON.stringify(state.user));
-    localStorage.setItem("isLoggedIn", "true");
-  } else {
-    localStorage.removeItem("user");
-    localStorage.setItem("isLoggedIn", "false");
+  // 초기 상태인지 확인
+  const isInitialState =
+    state.isLoggedIn === false && state.user === null && state.path === "/";
+
+  if (!isInitialState) {
+    if (state.isLoggedIn && state.user) {
+      localStorage.setItem("user", JSON.stringify(state.user));
+      localStorage.setItem("isLoggedIn", "true");
+    } else {
+      localStorage.removeItem("user");
+      localStorage.setItem("isLoggedIn", "false");
+    }
+    localStorage.setItem("path", state.path);
   }
-  localStorage.setItem("path", state.path);
 }
 
 userContext.subscribe(syncLocalStorage);
