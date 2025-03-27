32099a37b89c17bbb450505b3cc5e2c27839eacf
minhee <jmh3699@naver.com>
2025-03-28 01:43:22 +0900

    feat: 헤더 내비 스타일 원본으로 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,7 @@
 import { state } from "../state.js";
 
 export const Header = () => {
-  const headerElem = document.createElement("header");
-  headerElem.classList.add(
-    "bg-blue-600",
-    "text-white",
-    "p-4",
-    "sticky",
-    "top-0",
-  );
+  const headerDivElem = document.createElement("div");
 
   const currPath = window.location.pathname;
   const makeLiClass = (href) => {
@@ -18,8 +11,10 @@ export const Header = () => {
   const render = () => {
     const isProduction = import.meta.env.MODE === "production";
     const BASE = isProduction ? "/front_5th_chapter1-1" : "";
-    headerElem.innerHTML = `
-      <h1 class="text-2xl font-bold">항해플러스</h1>
+    headerDivElem.innerHTML = `
+      <header class="bg-blue-600 text-white p-4 sticky top-0">
+        <h1 class="text-2xl font-bold">항해플러스</h1>
+      </header>
       <nav class="bg-white shadow-md p-2 sticky top-14">
           <ul class="flex justify-around">
           ${
@@ -41,5 +36,5 @@ export const Header = () => {
 
   render();
   state.subscribe(render);
-  return headerElem;
+  return headerDivElem;
 };
