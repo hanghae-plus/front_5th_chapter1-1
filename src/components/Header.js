bf2d469bedd61d5918587401d246d0d633419e5f
sungchan.kim <sungchan.kim@datastreams.co.kr>
2025-03-27 17:09:54 +0900

    Fix: Header 색 변경 조건문 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,18 @@
 import auth from "../auth";
 
 export default () => {
+  const onMainPage = () => {
+    if (location.hash) {
+      return location.hash === "#/";
+    }
+    return location.pathname === "/";
+  };
+  const onProfilePage = () => {
+    if (location.hash) {
+      return location.hash === "#/profile";
+    }
+    return location.pathname === "/profile";
+  };
   return `
         <header class="bg-blue-600 text-white p-4 sticky top-0">
         <h1 class="text-2xl font-bold">항해플러스</h1>
@@ -8,8 +20,8 @@ export default () => {
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" class="${location.pathname === "/" ? "text-blue-600 font-bold" : "text-gray-600"}">홈</a></li>
-          <li><a href="/profile" class="${location.pathname === "/profile" ? "text-blue-600 font-bold" : "text-gray-600"}">프로필</a></li>
+          <li><a href="/" class="${onMainPage() ? "text-blue-600 font-bold" : "text-gray-600"}">홈</a></li>
+          <li><a href="/profile" class="${onProfilePage() ? "text-blue-600 font-bold" : "text-gray-600"}">프로필</a></li>
           ${
             auth.loggedIn
               ? `<li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>`
