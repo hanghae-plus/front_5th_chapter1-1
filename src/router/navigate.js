3c0b5f6d27ce7675aed402f7155de24f52bd1002
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2025-03-28 06:05:39 +0900

    fix: navigate 수정

--- src/router/navigate.js
+++ src/router/navigate.js
@@ -1,8 +1,10 @@
 import { userContext } from "../context/userContext.js";
 
 export function navigateTo(path) {
-  if (window.location.pathname !== path) {
-    window.history.pushState({}, "", path);
-    userContext.setState({ path });
+  const basePath = window.location.pathname.split("/").slice(0, -1).join("/");
+  const fullPath = `${basePath}${path}`;
+  if (window.location.pathname !== fullPath) {
+    window.history.pushState({}, "", fullPath);
+    userContext.setState({ path: fullPath });
   }
 }
