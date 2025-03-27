16ab092766ec35f37ca79f2ec45b2bded1bb6032
hanbeulYou <hanbeul.you@gmail.com>
2025-03-28 01:51:07 +0900

    refactor: Header 네비게이션 부분 상수화

---
+++ src/consts/navigation.js
@@ -0,0 +1,18 @@
+export const NAV_ITEMS = {
+  HOME: {
+    path: "/",
+    text: "홈",
+  },
+  PROFILE: {
+    path: "/profile",
+    text: "프로필",
+  },
+  LOGIN: {
+    path: "/login",
+    text: "로그인",
+  },
+  LOGOUT: {
+    path: "#",
+    text: "로그아웃",
+  },
+};
