0a92aa269e65264cfb8a7b7722e0b54f0e82fc45
Lee MinGi <mingi3442@gmail.com>
2025-03-28 02:42:48 +0900

    🐛 Bug fix

--- src/shared/config/index.js
+++ src/shared/config/index.js
@@ -1,4 +1,4 @@
 export const config = {
   basePath:
-    process.env.NODE_ENV === "production" ? "/front_5th_chapter1-1/" : "/",
+    process.env.NODE_ENV === "production" ? "/front_5th_chapter1-1" : "",
 };
