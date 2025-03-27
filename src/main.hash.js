d93bc384242c679c737e47ad3c2b78374b494da6
dlsxody1 <dlsxody1@naver.com>
2025-03-27 19:40:42 +0900

    chore : 라우팅 고치기

--- src/main.hash.js
+++ src/main.hash.js
@@ -1,31 +1,31 @@
-export class HashRouter {
-  constructor(routes) {
-    this.routes = routes;
-    this.container = document.body.querySelector("#root");
-  }
+import { HashRouter } from "./router/HashRouter.js";
+import MainPage from "./pages/MainPage.js";
+import LoginPage from "./pages/LoginPage.js";
+import ProfilePage from "./pages/ProfilePage.js";
+import ErrorPage from "./pages/ErrorPage.js";
+import { setupNavLogout } from "./components/Nav.js";
 
-  render(path = this.getPath()) {
-    const route = this.routes[path] || this.routes["default"];
-    route.render(this.container);
-  }
+const routes = {
+  "/": {
+    render: MainPage,
+    auth: false,
+  },
+  "/login": {
+    render: LoginPage,
+    auth: false,
+  },
+  "/profile": {
+    render: ProfilePage,
+    auth: true,
+  },
+  default: {
+    render: ErrorPage,
+    auth: false,
+  },
+};
 
-  start() {
-    if (!window.location.hash) {
-      window.location.hash = "#/";
-    }
-    this.render();
-    window.addEventListener("hashchange", () => this.render());
-  }
+window.setupNavLogout = setupNavLogout;
 
-  navigate(path) {
-    const currentPath = this.getPath();
-    if (this.routes[path] !== this.routes[currentPath]) {
-      window.location.hash = path.startsWith("/") ? path : `/${path}`;
-      this.render(path);
-    }
-  }
-
-  getPath() {
-    return window.location.hash.slice(1) || "/";
-  }
-}
+const router = new HashRouter(routes);
+window.router = router;
+router.start();
