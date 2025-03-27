25b4d35f96682135be2c3e55bc6b640128eccf9a
yoojinDev <yj.oh@teamsparta.co>
2025-03-27 23:33:30 +0900

    gh pages 배포를 위한 base 속성 추가

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/front_5th_chapter1-1/",
   test: {
     globals: true,
     environment: "jsdom",
