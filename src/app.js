3f795089b13ba7888955ef5edfaccadc37a60394
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2025-03-28 07:55:37 +0900

    fix: 배포 테스트

--- src/app.js
+++ src/app.js
@@ -5,9 +5,18 @@ export function renderApp() {
   const state = userContext.getState();
   const result = resolveRoute(state.path);
 
+  // 리다이렉트가 필요하면, baseUrl을 추가해서 경로를 올바르게 수정
   if (typeof result === "object" && result.redirect) {
-    window.history.pushState({}, "", result.redirect);
-    userContext.setState({ path: result.redirect });
+    const { redirect } = result;
+    const isGithubPages = window.location.origin.includes("github.io");
+
+    // 배포 환경인 경우 baseUrl을 포함한 경로로 리다이렉트 처리
+    const redirectTo = isGithubPages
+      ? window.location.origin + "/front_5th_chapter1-1" + redirect
+      : redirect; // 로컬 환경에서는 baseUrl 없이 상대 경로로 처리
+
+    window.history.pushState({}, "", redirectTo);
+    userContext.setState({ path: redirectTo });
     return;
   }
 
