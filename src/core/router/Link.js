4d021dce17e645c5fbb13e4deea249fa613deaca
Seonu <seonup95@gmail.com>
2025-03-28 00:47:05 +0900

    feat: 이벤트 처리 구현

--- src/core/router/Link.js
+++ src/core/router/Link.js
@@ -1,5 +1,17 @@
+import { addEvent } from "../../event";
+import { navigate } from "./router";
+
 const Link = ({ to, label, className = "" }) => {
-  return `<a href="${to}" class="${className}">${label}</a>`;
+  const eventKey = addEvent({
+    tag: "a",
+    type: "click",
+    listener: (e) => {
+      e.preventDefault();
+      navigate(to);
+    },
+  });
+
+  return `<a href="${to}" class="${className}" data-event="${eventKey}">${label}</a>`;
 };
 
 export default Link;
