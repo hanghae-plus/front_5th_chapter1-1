22caf1eb0fc72eeb089f81d288a84050b39b12d0
hanbeulYou <hanbeul.you@gmail.com>
2025-03-28 01:09:44 +0900

    refactor: 라우터 가드 관심사 분리

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,23 +1,20 @@
 import MainPage from "../pages/Main";
 import ProfilePage from "../pages/Profile";
 import LoginPage from "../pages/Login";
-import { AuthAPI } from "../interfaces/auth.interface";
 import { BASE_PATH } from "../consts/path";
+import { routeGuardService } from "../services/routeGuard.service";
 
 const routes = {
   [BASE_PATH]: MainPage,
   [BASE_PATH + "profile"]: () => {
-    if (!AuthAPI.isLoggedIn()) {
-      return { redirect: BASE_PATH + "login" };
-    }
+    const guard = routeGuardService.checkProfileAccess();
+    if (guard) return guard;
     return ProfilePage();
   },
   [BASE_PATH + "login"]: () => {
-    if (AuthAPI.isLoggedIn()) {
-      return { redirect: BASE_PATH };
-    }
+    const guard = routeGuardService.checkLoginAccess();
+    if (guard) return guard;
     return LoginPage();
   },
 };
-
 export default routes;
