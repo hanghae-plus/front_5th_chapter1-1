b98bc337e17150b61f8af1de2f7f8c9f9db1b097
alchogh <alchogh01@gmail.com>
2025-03-27 20:04:12 +0900

    add : env 파일, config 설정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
 
-export default defineConfig({
-  base: "/front_5th_chapter1-1",
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_APP_BASE,
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          hash: resolve(__dirname, "index.hash.html"),
+        },
+      },
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js",
+      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    },
+  };
 });
