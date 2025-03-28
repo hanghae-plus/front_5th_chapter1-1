37d6ead2b58b42d8bc1aa01ee4131f26e9d78133
sj.kim <sj.kim@greendote.co.kr>
2025-03-28 11:08:13 +0900

    fix. gh-pages 404 error 처리 코드 수정

--- src/main.hash.js
+++ src/main.hash.js
@@ -1,5 +1,3 @@
 import { initHashRouter } from "./routers/hashRouter";
 
 initHashRouter();
-
-console.log(location.hash);
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     base: env.VITE_BASE_PATH || "/",
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          hash: resolve(__dirname, "index.hash.html"),
+          404: resolve(__dirname, "404.html"),
+        },
+      },
+    },
     test: {
       globals: true,
       environment: "jsdom",
