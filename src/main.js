ff5339f7d8d7bd40232b44b13611e7e65ea41d75
sj.kim <sj.kim@greendote.co.kr>
2025-03-28 13:27:42 +0900

    chore. vite config 수정

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
 import { initRouter } from "./routers/router";
 
-console.log(import.meta.env.dev);
 initRouter();
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vitest/config";
 import { resolve } from "path";
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    base: env.VITE_BASE_PATH || "/",
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, "index.html"),
-          hash: resolve(__dirname, "index.hash.html"),
-          404: resolve(__dirname, "404.html"),
-        },
+export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter1-1/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        hash: resolve(__dirname, "index.hash.html"),
+        404: resolve(__dirname, "404.html"),
       },
     },
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.js",
-      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-    },
-  };
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+  },
 });
