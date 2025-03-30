c59dbc68e4183699d5e4be43846ea3ddf3a20bfc
Heejin <dev.heejiin@gmail.com>
2025-03-30 16:17:32 +0900

    fix: fix workflow

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
-import renderPage, { navigateTo } from "./app/router";
+import renderPage from "./app/router";
 
 document.addEventListener("DOMContentLoaded", () => {
   renderPage();
-
-  document.body.addEventListener("click", (event) => {
-    const target = event.target;
-
-    if (target.matches("[data-link]")) {
-      event.preventDefault();
-      const url = target.dataset.link;
-      navigateTo(url);
-    }
-  });
 });
